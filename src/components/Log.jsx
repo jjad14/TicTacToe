a6f9f5e0282f42c0e5c7fb1d9c24c96f8384dbd1
Dennis <dmarlo96@live.ca>
2025-05-21 14:03:42 -0400

    Game Log Component

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,3 +1,15 @@
-export const Log = () => {
-	return <ol id='log'></ol>;
+export const Log = ({ turns }) => {
+	return (
+		<>
+			<h2>Game Log</h2>
+			<ol id='log'>
+				{turns.map((turn) => (
+					<li key={`${turn.square.row}${turn.square.col}`}>
+						{turn.player} selected {turn.square.row},{' '}
+						{turn.square.col}
+					</li>
+				))}
+			</ol>
+		</>
+	);
 };
